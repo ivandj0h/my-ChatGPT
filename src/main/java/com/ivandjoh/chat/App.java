90e3043e4a49c0abbd45a888cc1ec8502dbacc10
ivandjoh <ivandi.djoh@gmail.com>
2023-02-09 03:10:48 +0800

    updating chat ServiceGPT

--- src/main/java/com/ivandjoh/chat/App.java
+++ src/main/java/com/ivandjoh/chat/App.java
@@ -3,10 +3,12 @@ package com.ivandjoh.chat;
 import com.ivandjoh.chat.service.ServiceGPT;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class App {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		ServiceGPT.serviceExec();
 	}
 
