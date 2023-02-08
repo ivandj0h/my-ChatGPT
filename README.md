20b3742e396c430daf85de0d929dd880c3575a78
ivandjoh <ivandi.djoh@gmail.com>
2023-02-09 02:02:47 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# My ChatGPT
+
+## Description
+This is a Java CLI Application that talks to the [ChatGPT](https://chat.openai.com/auth/login)
+
+
+```bash
+    This CLI Still in Development, kindly wait and report any bugs
+```
---
+++ src/test/java/com/ivandjoh/chat/MyChatgptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivandjoh.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyChatgptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
